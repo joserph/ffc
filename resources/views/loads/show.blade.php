e528a81aefaa6f55434350275bce8ea90cb7dde4
Jose Perez <joserph.a@gmail.com>
2019-07-29 20:53:28 -0500

    create migrate sketch

--- resources/views/loads/show.blade.php
+++ resources/views/loads/show.blade.php
@@ -29,6 +29,27 @@
                 </a>
             </div>
         </div>
+        <div class="col-md-4">
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <i class="fas fa-map-marked-alt fa-7x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                            <h3>CROQUIS CONTENEDOR</h3>
+                        </div>
+                    </div>
+                </div>
+                <a href="{{ route('pallets.index', $load->code) }}" data-toggle="tooltip" data-placement="bottom" title="Distribución de pallets">
+                    <div class="panel-footer">
+                        <span class="pull-left"><i class="fa fa-eye fa-fw"></i> Ver Detalles</span>
+                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
