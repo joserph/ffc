1e783a831de2d5867d0f764b1a0ca628c136c25b
Jose Perez <joserph.a@gmail.com>
2019-12-25 21:41:06 -0500

    table in pallet items pdf

--- resources/views/pallets/pdf.blade.php
+++ resources/views/pallets/pdf.blade.php
@@ -5,21 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title></title>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
 </head>
 <body>
     <table>
-        <thead>
-            <tr>
-                <th>Exporter</th>
-                <th>Hawb</th>
-                <th>PCS</th>
-                <th>BXS</th>
-                <th>FB</th>
-                <th>HB</th>
-                <th>QB</th>
-                <th>EB</th>
-            </tr>
-        </thead>
+        
         @php
             $idfarm = 0;
             $total_farm = 0;
@@ -27,33 +22,38 @@
         @foreach($clients_all as $client)
             <thead>
                 <tr class="success">
-                    <th colspan="8" class="text-center">{{ strtoupper($client->name) }}</th>
+                    <th>Awb:</th>
+                    <th colspan="6" class="text-center">{{ strtoupper($client->name) }}</th>
+                </tr>
+                <tr>
+                    <th>Exporter</th>
+                    <th>Hawb</th>
+                    <th>PCS</th>
+                    <th>BXS</th>
+                    <th>HB</th>
+                    <th>QB</th>
+                    <th>EB</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($pallet_items as $item)
                     @if($item->id_client == $client->id)
-                        {{-- Agrupar fincas iguales --}}
-                        
-                        
-                            <tr>
-                                <td>{{ $item->id }}</td>
-                                <td>
-                                    @foreach ($farms as $item_farm)
-                                        @if ($item->id_farm == $item_farm->id)
-                                            {{ $item_farm->name }}
-                                        @endif
-                                    @endforeach
-                                </td>
-                                <td>{{ $item->quantity }}</td>
-                            </tr>
-                            
-                        @php
-                            $idfarm = $item->id_farm;
-                        @endphp
-                        
+                        <tr>
+                            <td>
+                                @foreach ($farms as $item_farm)
+                                    @if ($item->id_farm == $item_farm->id)
+                                        {{ $item_farm->name }}
+                                    @endif
+                                @endforeach
+                            </td>
+                            <td>123456</td>
+                            <td>{{ $item->quantity }}</td>
+                            <td>22.50</td>
+                            <td>{{ $item->hb }}</td>
+                            <td>{{ $item->qb }}</td>
+                            <td>{{ $item->eb }}</td>
+                        </tr>
                     @endif
-                    
                 @endforeach
                 
             </tbody>
